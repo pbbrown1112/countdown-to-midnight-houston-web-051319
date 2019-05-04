e21caf991d6ef07b1b174ac722d4394892badd60
Brendan Brown <pbbrown1112@gmail.com>
2019-05-04 12:09:06 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num_input)
+  number = num_input.to_i
+  while number > 0
+      phrase = "#{number} SECOND(S)!\n"
+      puts phrase
+      number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num_input2)
+  number = num_input2.to_i
+  while number > 0
+    sleep 1
+    number -=1
+  end
 end
